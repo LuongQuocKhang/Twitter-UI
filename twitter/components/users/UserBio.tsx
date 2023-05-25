b541cfab0a0083f86ad355395abd42a43bdcb33b
O2F-IT\luong.quockhang_amar <luong.quockhang@amaris.com>
2023-05-25 17:02:56 +0700

    update

--- twitter/components/users/UserBio.tsx
+++ twitter/components/users/UserBio.tsx
@@ -1,3 +1,6 @@
+import { format } from 'date-fns';
+import Button from '../forms/Button';
+
 interface UserBioProps {
     userId: string
 }
@@ -5,6 +8,11 @@ const UserBio: React.FC<UserBioProps> = ({
     userId
 }) => {
     return ( <>
+        <div className='border-b-[1px] border-neutral-800 pb-4'>
+            <div className='flex justify-end p-2'>
+                <Button secondary label='Follow' onClick={() => {}} />
+            </div>
+        </div>
     </> );
 }
  
